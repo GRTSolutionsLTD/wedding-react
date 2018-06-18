50c32e54a52191ff4ad9368a8e1397f235e35fa3
Rivki Aizen <rivki@grth.co.il>
2018-06-18 10:30:58 +0300

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Navigation = ({ className, buttonClassName }) =>
+  <nav className={className}>
+    <Link className={buttonClassName} to="/">
+      Home
+    </Link>
+    {/* reduser  בלינק מקשרים ל */}
+    <Link className={buttonClassName} to="todo">
+      Todo
+    </Link>
+    <Link className={buttonClassName} to="weather">
+      Weather
+    </Link>
+  </nav>
+
+Navigation.defaultProps = {
+  className: '',
+  buttonClassName: 'c-button'
+}
+
+export default Navigation
